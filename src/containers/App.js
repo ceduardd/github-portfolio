d3c48a208ecd7fbcd34f68c9e318ac646d793ae0
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-07-22 21:26:39 -0500

    project structure

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import logo from '../logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />
+    </div>
+  );
+}
+
+export default App;
