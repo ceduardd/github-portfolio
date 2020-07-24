3fa9ecddb99b450704640b3f7989640964c1d3a3
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-07-24 16:34:51 -0500

    apply styles with styled-components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../components/Header/Header';
+import Profile from './Profile';
 import logo from '../logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <Header logo={logo} />
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppWrapper>
+          <Header logo={logo} />
+          <Profile />
+        </AppWrapper>
+      </>
+    );
+  }
 }
 
 export default App;
